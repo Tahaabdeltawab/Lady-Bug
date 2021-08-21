2124767436e18ba22bd29fe9c530922ffb548232
Tahaabdeltawab <tahaabdo252000@gmail.com>
2021-08-21 23:05:52 +0200

    complete sharing posts

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -16,30 +16,33 @@ class PostResource extends JsonResource
      */
     public function toArray($request)
     {
-        $farm = (new FarmRepository(app()))->find($this->farm_id);
-        $post_type = (new PostTypeRepository(app()))->find($this->post_type_id);
+        $post = $this->shared == null ? $this : $this->shared;
+        $farm = (new FarmRepository(app()))->find($post->farm_id);
+        $post_type = (new PostTypeRepository(app()))->find($post->post_type_id);
 
-        return [
+        $return = [
             'id' => $this->id,
-            'status' => $this->status,
-            'content' => $this->content,
+            'status' => $post->status,
+            'content' => $post->content,
             'author' => new UserResource($this->author),
             'farmed_type_photo' => @$farm->farmed_type->asset->asset_url,
-            'farmed_type_id' => $this->farmed_type_id ?? @$farm->farmed_type_id ,
-            'solved' => $this->solved,
-            'image_assets' => collect($this->assets)->whereIn('asset_mime', config('myconfig.image_mimes'))->pluck('asset_url')->all(),
-            'video_assets' => collect($this->assets)->whereIn('asset_mime', config('myconfig.video_mimes'))->pluck('asset_url')->all(),
+            'farmed_type_id' => $post->farmed_type_id ?? @$farm->farmed_type_id ,
+            'solved' => $post->solved,
+            'image_assets' => collect($post->assets)->whereIn('asset_mime', config('myconfig.image_mimes'))->pluck('asset_url')->all(),
+            'video_assets' => collect($post->assets)->whereIn('asset_mime', config('myconfig.video_mimes'))->pluck('asset_url')->all(),
             'post_type' => @new PostTypeResource($post_type), // @ because the 'farm' post type does not return by the global scope in the PostType model, so this will be null if the post type is 4 (farm)
-            'likers_count' => $this->likers->count(),
-            'dislikers_count' => $this->dislikers->count(),
-            'comments_count' => $this->comments->count(),
-            'likers' => [],//$this->when( auth()->id() == $this->author_id ,UserResource::collection($this->likers)),
-            'dislikers' => [],//$this->when( auth()->id() == $this->author_id ,UserResource::collection($this->dislikers)),
-            'liked_by_me' => $this->likers->where('id', auth()->id())->count() ? true : false ,
-            'disliked_by_me' => $this->dislikers->where('id', auth()->id())->count() ? true : false ,
-            'comments' => $request->routeIs('api.posts.show') ? CommentResource::collection($this->comments->whereNull('parent_id')) : [],
-            'created_at' => $this->created_at->diffForHumans(),
-            'shared' => new PostResource($this->shared),
+            'likers_count' => $post->likers->count(),
+            'dislikers_count' => $post->dislikers->count(),
+            'comments_count' => $post->comments->count(),
+            'likers' => [],//$post->when( auth()->id() == $post->author_id ,UserResource::collection($post->likers)),
+            'dislikers' => [],//$post->when( auth()->id() == $post->author_id ,UserResource::collection($post->dislikers)),
+            'liked_by_me' => $post->likers->where('id', auth()->id())->count() ? true : false ,
+            'disliked_by_me' => $post->dislikers->where('id', auth()->id())->count() ? true : false ,
+            'comments' => $request->routeIs('api.posts.show') ? CommentResource::collection($post->comments->whereNull('parent_id')) : [],
+            'created_at' => $post->created_at->diffForHumans(),
+            'shared' => $this->shared == null ? null : new UserResource($this->shared->author),
         ];
+
+        return $return;
     }
 }
