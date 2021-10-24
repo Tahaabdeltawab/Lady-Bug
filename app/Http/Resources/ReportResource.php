acb20e6fc5e0758c9a258547c9fab5b44b2a80f9
Tahaabdeltawab <tahaabdo252000@gmail.com>
2021-10-24 18:16:23 +0200

    return post in single report response

--- app/Http/Resources/ReportResource.php
+++ app/Http/Resources/ReportResource.php
@@ -19,6 +19,7 @@ class ReportResource extends JsonResource
             'report_type_id' => $this->report_type_id,
             'reportable_type' => $this->reportable_type,
             'reportable_id' => $this->reportable_id,
+            'post' => $this->when($request->report, $this->reportable instanceof \App\Models\Post ? new PostResource($this->reportable) : $this->reportable),
             'reporter_id' => $this->reporter_id,
             'description' => $this->description,
             'status' => $this->status,
