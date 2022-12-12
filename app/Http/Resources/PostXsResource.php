0f596ee80c16aeef521be9aea16f06bbe6118fe4
tahaabdeltawab <tahaabdo252000@gmail.com>
2022-12-13 00:09:48 +0200

    add share_created_at to postXsRes

--- app/Http/Resources/PostXsResource.php
+++ app/Http/Resources/PostXsResource.php
@@ -16,10 +16,12 @@ class PostXsResource extends JsonResource
      */
     public function toArray($request)
     {
-        $notShared = $this->shared == null;
-        $post = $notShared ? $this : $this->shared;
-        // $farm = (new FarmRepository(app()))->find($post->farm_id);
-        $post_type = (new PostTypeRepository(app()))->find($post->post_type_id);
+        // $this always refers to the newer post either original or shared
+        // $this->shared refers to the original post
+        // $post always refers to an original post (not shared or original of the shared)
+        // post is original (not shared) if it has no original post ($this->shared == null)
+        $original = $this->shared == null;
+        $post = $original ? $this : $this->shared;
 
         $return = [
             'id' => $this->id,
@@ -28,7 +30,7 @@ class PostXsResource extends JsonResource
             'solved' => $post->solved,
             'image_assets' => collect($post->assets)->whereIn('asset_mime', config('myconfig.image_mimes'))->pluck('asset_url')->all(),
             'video_assets' => collect($post->assets)->whereIn('asset_mime', config('myconfig.video_mimes'))->pluck('asset_url')->all(),
-            'post_type' => @new PostTypeResource($post_type), // @ because the 'farm' post type does not return by the global scope in the PostType model, so this will be null if the post type is 4 (farm)
+            'post_type' => @new PostTypeResource($post->post_type), // @ because the 'farm' post type does not return by the global scope in the PostType model, so this will be null if the post type is 4 (farm)
             'comments_count' => $post->comments->count(),
             'likers_count' => $post->likers->count(),
             'dislikers_count' => $post->dislikers->count(),
@@ -37,9 +39,11 @@ class PostXsResource extends JsonResource
             'comments' => $request->routeIs('api.posts.show') ? CommentResource::collection($post->comments->whereNull('parent_id')) : [],
             'created_at' => $post->created_at->diffForHumans(),
             'created_date' => $post->created_at,
-            'shared' => $notShared ? null : new UserResource($this->shared->author),
+            'share_created_at' => $original ? null : $this->created_at->diffForHumans(),
+            'share_created_date' => $original ? null : $this->created_at,
+            'shared' => $original ? null : new UserResource($this->shared->author), // sharer
+            'shared_content' => $original ? null : $this->content, // content added from the sharer
             'share_id' => $post->id, // used to share the post. if this is an original post its value will be $this->id, if it's a shared post , its value will be the original post id
-            'shared_content' => $notShared ? null : $this->content, // content added from the sharer
             'business' => BusinessMdResource::make($this->business),
         ];
 
